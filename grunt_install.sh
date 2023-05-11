2411d883b905391f0610fb8e55509bbe1e69a311
mtegarsantosa <muhammad.tegar@play3.gg>
2023-05-11 21:17:48 +0700

    initial commit

---
+++ grunt_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Installation steps needed to install grunt and successfully compile translations:
+
+#npm install "grunt"
+#npm install "grunt-contrib-uglify"
+#npm install "grunt-contrib-concat"
+#npm install "grunt-contrib-watch"
+#npm install "grunt-css"
+#npm install "grunt-markdown"
+#npm install "grunt-macreload"
+#npm install "grunt-angular-gettext"
+#npm install "bower"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+nvm use v4
+npm install
+./node_modules/.bin/bower install
+
+# extract: ./node_modules/.bin/grunt nggettext_extract 
+
+# This action will create a template.pot file in po/ folder. You can open it with some PO editor (Poedit). 
+# Read this http://angular-gettext.rocketeer.be/dev-guide/translate/ guide to learn how to edit/update/import PO files from a generated POT file. 
+# PO file will be generated inside po/ folder.
+# If you make new changes, simply run grunt compile again to generate a new .pot template 
+# and the angular javascript js/translations.js. Then (if use Poedit), open .po file and choose update from POT File from Catalog menu.
+
+# Don't forget to add your language in public/src/js/controllers/footer.js  (!)
+
+# launch: ./node_modules/.bin/grunt compile
